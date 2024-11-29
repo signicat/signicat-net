8c72d1c2dc90bd1a8ee401b0ed4ccd21c7f44aa9
vPolland <vegard@idfy.io>
2024-11-29 09:33:44 +0100

    Express sign: add new extra info type, remove unused variants

--- src/Signicat.SDK/Services/Signing/Express/Entities/ExtraInfoSignerType.cs
+++ src/Signicat.SDK/Services/Signing/Express/Entities/ExtraInfoSignerType.cs
@@ -5,19 +5,7 @@ namespace Signicat.Services.Signing.Express.Entities
 {
     public enum ExtraInfoSignerType
     {
-        [Obsolete("Please use vippsAmlPerson")]
-        [EnumMember(Value = "bankIDApisAmlPersonSanctionPep")]
-        BankIDApisAmlPersonSanctionPep = 6,
-    
-        [Obsolete("Please use vippsAmlPerson")]
-        [EnumMember(Value = "bankIDApisAmlCurrentAddress")]
-        BankIDApisAmlCurrentAddress = 7,
-        
-        [Obsolete("Please use vippsAmlPerson")]
-        [EnumMember(Value = "bankIDApisAmlPersonSanctionPepReport")]
-        BankIDApisAmlPersonSanctionPepReport = 8, 
-        
-        [EnumMember(Value = "vippsAmlPerson")]
-        VippsAmlPerson = 8,
+        [EnumMember(Value = "personLookup")]
+        PersonLookup = 10,
     }
 }
