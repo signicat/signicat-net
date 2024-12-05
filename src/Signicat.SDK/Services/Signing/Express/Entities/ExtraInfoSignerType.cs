0fc15fe31ee4d04b0433e6e8eca62ad51f917893
vPolland <vegard@idfy.io>
2024-12-05 09:39:07 +0100

    Mark unused Extrainfo types as obsolete instead of removing them entirely

--- src/Signicat.SDK/Services/Signing/Express/Entities/ExtraInfoSignerType.cs
+++ src/Signicat.SDK/Services/Signing/Express/Entities/ExtraInfoSignerType.cs
@@ -5,6 +5,22 @@ namespace Signicat.Services.Signing.Express.Entities
 {
     public enum ExtraInfoSignerType
     {
+        [Obsolete("Please use PersonLookup")]
+        [EnumMember(Value = "bankIDApisAmlPersonSanctionPep")]
+        BankIDApisAmlPersonSanctionPep = 6,
+
+        [Obsolete("Please use PersonLookup")]
+        [EnumMember(Value = "bankIDApisAmlCurrentAddress")]
+        BankIDApisAmlCurrentAddress = 7,
+
+        [Obsolete("Please use PersonLookup")]
+        [EnumMember(Value = "bankIDApisAmlPersonSanctionPepReport")]
+        BankIDApisAmlPersonSanctionPepReport = 8, 
+
+        [Obsolete("Please use PersonLookup")]
+        [EnumMember(Value = "vippsAmlPerson")]
+        VippsAmlPerson = 8,
+        
         [EnumMember(Value = "personLookup")]
         PersonLookup = 10,
     }
