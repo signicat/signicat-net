15873115ffd818a48a9c9e01a9db169caac45a9d
Rune Synnevåg <rune@idfy.io>
2025-03-24 14:10:15 +0100

    Create UsageServiceHelper
    Made a extension class to make it easier to get next page

---
+++ src/Signicat.SDK/Services/Usage/UsageServiceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Signicat.Entities;
+
+namespace Signicat.Services.Usage;
+
+public static class UsageServiceHelper
+{
+    public static async Task<UsagePaginationResult> GetNextAsync(this IUsageService usageService, UsagePaginationResult result)
+    {
+        return result.Next is null ? null : await usageService.GetUsageAsync(result.Next, 100);
+    }
+    
+    public static UsagePaginationResult GetNext(this IUsageService usageService, UsagePaginationResult result)
+    {
+        return result.Next is null ? null : usageService.GetUsage(result.Next, 100);
+    }
+}
